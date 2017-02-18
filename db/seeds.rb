f5cae576bae74f38e6a8fd21112cae9bcc5f2290
arthurnfarias <anf2@cin.ufpe.br>
2017-02-18 16:21:53 -0300

    Mudanças gerais

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 User.create(email: "anf2@cin.ufpe.br", password: "264895")
+
+Paciente.create(nome: "Arthur Farias", indentidade: "555939", cpf: "09731650490", email: "anf2@cin.ufpe.br", telefone: "999658697")
